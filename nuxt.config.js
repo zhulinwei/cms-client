f3731c8f12e183c03011f335bdee2cb64e22c596
Level.Z <1171567018@qq.com>
2018-05-27 15:05:21 +0800

    update cms

---
+++ nuxt.config.js
@@ -0,0 +1,44 @@
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'cms',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Build configuration
+  */
+  css: [
+    'element-ui/lib/theme-chalk/index.css'
+  ],
+  plugins: [
+    '@/plugins/element-ui'
+  ],
+  build: {
+    /*
+    ** Run ESLint on save
+    */
+    extend (config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <p>这里是首页</p>
+</template>
